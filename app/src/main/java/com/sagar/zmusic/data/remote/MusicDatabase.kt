9092dab1f182f68d937e9ed5076d3cb1fd943bb2
sagar <sagarbisht509@gmail.com>
2023-02-02 23:47:04 +0530

    adding firestore

--- app/src/main/java/com/sagar/zmusic/data/remote/MusicDatabase.kt
+++ app/src/main/java/com/sagar/zmusic/data/remote/MusicDatabase.kt
@@ -1,10 +1,21 @@
 package com.sagar.zmusic.data.remote
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.sagar.zmusic.data.entities.Song
 import com.sagar.zmusic.others.Constant.SONG_COLLECTION
+import kotlinx.coroutines.tasks.await
 
 class MusicDatabase {
 
     private val firestore = FirebaseFirestore.getInstance()
     private val songCollection = firestore.collection(SONG_COLLECTION)
+
+    // this fun is suspend bxz we did not want to execute this fun in main thread
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song:: class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
 }
