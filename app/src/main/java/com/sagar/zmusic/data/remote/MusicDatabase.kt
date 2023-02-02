33199eafc9235472684acab7b9485bbace2c1980
sagar <sagarbisht509@gmail.com>
2023-02-02 23:16:04 +0530

    adding firestore

--- app/src/main/java/com/sagar/zmusic/data/remote/MusicDatabase.kt
+++ app/src/main/java/com/sagar/zmusic/data/remote/MusicDatabase.kt
@@ -1,6 +1,10 @@
 package com.sagar.zmusic.data.remote
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sagar.zmusic.others.Constant.SONG_COLLECTION
+
 class MusicDatabase {
 
-    val firestore = ""
+    private val firestore = FirebaseFirestore.getInstance()
+    private val songCollection = firestore.collection(SONG_COLLECTION)
 }
