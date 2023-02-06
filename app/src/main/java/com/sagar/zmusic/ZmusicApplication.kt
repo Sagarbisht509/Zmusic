c349adfe8ca66fc2d4af22a691c8fa4099266d8b
sagar <sagarbisht509@gmail.com>
2023-02-06 23:36:45 +0530

    adding dagger

---
+++ app/src/main/java/com/sagar/zmusic/ZmusicApplication.kt
@@ -0,0 +1,9 @@
+package com.sagar.zmusic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ZmusicApplication : Application() {
+
+}
---
+++ app/src/main/java/com/sagar/zmusic/di/appModule.kt
@@ -0,0 +1,28 @@
+package com.sagar.zmusic.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.sagar.zmusic.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ActivityComponent::class)
+object appModule {
+
+    @Singleton
+    @Provides
+    fun porvideGlidInstance(@ApplicationContext context: Context) =
+        Glide.with(context).applyDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background)
+                .error(R.drawable.ic_launcher_background)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+        )
+}
